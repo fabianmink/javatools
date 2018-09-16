a42254de26e92af12dbbc0d51dc154dd869a3c66
Fabian Mink <fabian.mink@gmx.de>
2018-09-16 20:57:07 +0100

    now working

--- src/de/mink_ing/automation/blocks/AdvancedBlindController.java
+++ src/de/mink_ing/automation/blocks/AdvancedBlindController.java
@@ -4,25 +4,33 @@ package de.mink_ing.automation.blocks;
 //todo: Not working. Improve!!
 public class AdvancedBlindController extends BlindController {
 	
+	private enum states{
+		state_off,
+		state_runDown,
+		state_runUp,
+		state_jogDown,
+		state_jogUp,
+		state_wait,
+	}
 	
-	private int runTime = 1000;
-	private int minRunTime = 10;
+	states myState = states.state_off;
+	
+	
+	private final int runTime = 1000;
+	private final int jogTime = 2;
+	private final int waitTime = 10;
+	
+	private int timer;
 	
 	private boolean in_up;
 	private boolean in_dn;
-	private int running;
+
 	private boolean rem_up;
 	private boolean rem_dn;
 	
-	private Debounce db_up;
-	private Debounce db_dn;
 	
 	public AdvancedBlindController(int Ts) {
 		super(Ts);
-		db_up = new Debounce(Ts);
-		db_dn = new Debounce(Ts);
-		db_up.setDebounceTime(1000);
-		db_dn.setDebounceTime(1000);
 	}
 	
 	//todo:
@@ -38,32 +46,92 @@ public class AdvancedBlindController extends BlindController {
 	}
 	
 	public void oneStep(){
-		
-		
-		if(running > 0) {
-			running--;
-			if(db_up.getOutput() || db_dn.getOutput()) {
-				running = 0;
-				rem_up = false;
-				rem_dn = false;
+
+		switch(myState) {
+		case state_off:
+			rem_dn = false;
+			rem_up = false;
+			if(in_dn) {
+				timer = jogTime;
+				myState = states.state_jogDown;
 			}
+			if(in_up) {
+				timer = jogTime;
+				myState = states.state_jogUp;
+			}
+			break;
+		
+		case state_jogDown:
+			rem_dn = true;
+			if(timer > 0) {
+				timer--;
+				if(!in_dn) {
+					timer = runTime;
+					myState = states.state_runDown;
+				}
+			}
+			else {
+				if(!in_dn) {
+					myState = states.state_off;
+				}
+			}
+			break;
+		
+		case state_jogUp:
+			rem_up = true;
+			if(timer > 0) {
+				timer--;
+				if(!in_up) {
+					timer = runTime;
+					myState = states.state_runUp;
+				}
+			}
+			else {
+				if(!in_up) {
+					myState = states.state_off;
+				}
+			}
+			break;
+			
+		case state_runDown:
+			if(timer > 0) {
+				timer--;
+				if(in_up || in_dn) {
+					timer = waitTime;
+					myState = states.state_wait;
+				}
+			}
+			else {
+				myState = states.state_off;
+			}
+			break;
+			
+		case state_runUp:
+			if(timer > 0) {
+				timer--;
+				if(in_up || in_dn) {
+					timer = waitTime;
+					myState = states.state_wait;
+				}
+			}
+			else {
+				myState = states.state_off;
+			}
+			break;
+			
+		case state_wait:
+			rem_dn = false;
+			rem_up = false;
+			if(timer > 0) {
+				timer--;
+			}
+			else {
+				myState = states.state_off;
+			}
+		
+
 		}
-		else {
-			if(db_dn.getOutput()) {
-				running = runTime;
-				rem_dn = true;
-			}
-			else if(db_up.getOutput()) {
-				running = runTime;
-				rem_up = true;
-			}
-		}
-		
-		db_up.setInput(in_up);
-		db_dn.setInput(in_dn);
-		
-		db_up.oneStep();
-		db_dn.oneStep();
+
 		
 		super.setInputDn(rem_dn);
 		super.setInputUp(rem_up);
