b978aa8f0c2a650d49757699cca7bf509277e732
Fabian Mink <fabian.mink@gmx.de>
2018-10-13 21:18:23 +0100

    lock for run/updn corrected

--- src/de/mink_ing/automation/blocks/BlindController.java
+++ src/de/mink_ing/automation/blocks/BlindController.java
@@ -13,7 +13,7 @@ public class BlindController extends DynamicBlock {
 	//private int blindUpTime = 10;
 	//private int blindDownTime = 10;
 
-	private int lockTime = 10;
+	private int lockTime = 4;
 	private int updnTailTime = 100;
 
 
@@ -23,6 +23,8 @@ public class BlindController extends DynamicBlock {
 
 	private int lock_up;
 	private int lock_dn;
+	private int lock_run;
+	private int lock_updn;
 	private int updn_tail;
 
 
@@ -43,20 +45,59 @@ public class BlindController extends DynamicBlock {
 
 		if(lock_dn > 0) lock_dn--;
 		if(lock_up > 0) lock_up--;
-		if(updn_tail > 0) updn_tail--;
-		else updn = false;
+		if(lock_run > 0) lock_run--;
+		if(lock_updn > 0) lock_updn--;
 		
-		if(in_up && !in_dn) {
+		if(updn_tail > 0) updn_tail--;
+		
+		if((updn_tail == 0) && (updn == true)) {
+			if(lock_updn == 0) {
+				lock_run = lockTime;
+				updn = false;
+			}
+		}
+		
+		if(in_up && !in_dn) { //Up request
 			if(lock_up == 0) {
 				up = true;
 				lock_dn = lockTime;
 			}
+			
+			if(updn == true) {
+				updn_tail = updnTailTime;
+				if(lock_run == 0) {
+					run = true;
+					lock_updn = lockTime; //lock up/dn while running
+				}
+			}
+			else {
+				if(lock_updn == 0) {
+					updn = true;
+					updn_tail = updnTailTime;
+					lock_run = lockTime; //lock run after updn switching
+				}
+			}
+			
 		}
 
-		if(in_dn && !in_up) {
+		if(in_dn && !in_up) { //Down request
 			if(lock_dn == 0) {
 				dn = true;
 				lock_up = lockTime;
+			}
+			
+			
+			if(updn == false) {
+				if(lock_run == 0) {
+					run = true;
+					lock_updn = lockTime; //lock up/dn while running
+				}
+			}
+			else {
+				if(lock_updn == 0) {
+					updn = false;
+					lock_run = lockTime; //lock run after updn switching
+				}
 			}
 		}
 
@@ -67,16 +108,8 @@ public class BlindController extends DynamicBlock {
 			lock_up = lockTime;
 		}
 		
-		if(up) {
-			run = true;
-			updn = true;
-			updn_tail = updnTailTime;
-		}
-		
-		if(dn) {
-			run = true;
-			updn = false;
-			updn_tail = 0;
+		if(run) {
+			lock_updn = lockTime; //lock up/dn while running
 		}
 	}
 
