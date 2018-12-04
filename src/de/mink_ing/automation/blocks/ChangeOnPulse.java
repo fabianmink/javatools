c94a595b5d20e094ddcaa4e240fc2d32b3058386
Fabian Mink <fabian.mink@gmx.de>
2018-12-04 20:30:50 +0100

    force inputs

--- src/de/mink_ing/automation/blocks/ChangeOnPulse.java
+++ src/de/mink_ing/automation/blocks/ChangeOnPulse.java
@@ -8,21 +8,41 @@ public class ChangeOnPulse extends DynamicBlock{
 
 	//internal states
 	private boolean state = false;
-			
+	private boolean in_old;
+	
 	//inputs
 	private boolean in;
-	private boolean in_old;
+	private boolean forceOn;
+	private boolean forceOff;
 	
 	//updateStates
 	public void oneStep(){
-		if(in & !in_old) {
+		
+		
+		if(forceOff) { //Force off has highest prio
+			state = false; 
+			forceOff = false; //Reset force off (might be set again in next cycle)
+		}
+		else if(forceOn) { //Force on has prio over toggle
+			state = true; 
+			forceOff = false; //Reset force off (might be set again in next cycle)
+		}
+		else if(in & !in_old) { //Toggle has lowest prio
 			state = !state;
-		}				
+		}
 		in_old = in;
 	}
 	
 	public void setInput(boolean in){
 		this.in = in;
+	}
+	
+	public void forceOn(boolean in){
+		this.forceOn = in;
+	}
+	
+	public void forceOff(boolean in){
+		this.forceOff = in;
 	}
 	
 	public boolean getOutput(){
